03583164153bc6b126a83fcc6c98286c328c000c
slangoij <71580318+Slangoij@users.noreply.github.com>
2022-04-21 15:54:59 +0900

    Create_gugudan.java_220421
    웹 디렉에서 알고리즘 문제풀이 옮겨옴

---
+++ JAVA/2022KAKAO blind/gugudan.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.util.Map.Entry;
+
+public class gugudan {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        List<String> lst = Arrays.asList(report);
+        Set<String> set = new HashSet<String>(lst);
+        List<String> newList = new ArrayList<String>(set);
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        for (String str: newList) {
+        	String[] splitstr = str.split(" ");
+        	ArrayList<String> tmp = new ArrayList<>(Arrays.asList());
+    		ArrayList<String> tmplst = map.getOrDefault(splitstr[1], tmp);
+    		tmplst.add(splitstr[0]);
+    		map.put(splitstr[1], tmplst);
+        }
+
+        HashMap<String, Integer> reportmap = new HashMap<>();
+        for (Entry<String, ArrayList<String>> entryset : map.entrySet()) {
+        	ArrayList<String> nowlst = entryset.getValue();
+        	int nowsize = nowlst.size();
+        	if (nowsize >= k) {
+        		for (String nowkey:nowlst) {
+        			reportmap.put(nowkey, reportmap.getOrDefault(nowkey, 0) + 1);
+        		}
+        	}
+        }
+        
+        ArrayList<Integer> anslist = new ArrayList<>();
+        for (String id:id_list) {
+        	anslist.add(reportmap.get(id));
+        }
+        
+        int[] answer = new int[id_list.length];
+        for (int i=0;i<id_list.length;i++) {
+        	answer[i] = anslist.get(i);
+        }
+        
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+    	gugudan gu = new gugudan();
+    	
+    	String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+    	String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+    	int k = 2;
+    	
+    	System.out.println(gu.solution(id_list, report, k));
+    }
+}
