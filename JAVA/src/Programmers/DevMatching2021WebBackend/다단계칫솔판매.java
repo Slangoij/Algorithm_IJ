44d12611c9699fdd9d95cd6e7652d3f1949f2815
ij_slango <junginje@gmail.com>
2022-05-18 00:03:43 +0900

    다단계 칫솔판매 풀이중-인텔리제이 말썽이다_220517

--- JAVA/src/Programmers/DevMatching2021WebBackend/다단계칫솔판매.java
+++ JAVA/src/Programmers/DevMatching2021WebBackend/다단계칫솔판매.java
@@ -1,10 +1,10 @@
-package DevMatching2021WebBackend;
+package Programmers.DevMatching2021WebBackend;
 
 import java.util.*;
 
 public class 다단계칫솔판매 {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        int[] answer = {};
+        int[] answer = new int[enroll.length];
 
         HashMap<String, String> parentMap = new HashMap<>();
         HashMap<String, Integer> memIdxMap = new HashMap<>();
@@ -16,7 +16,14 @@ public class 다단계칫솔판매 {
 
         for (int i=0;i< seller.length;i++){
             String nowMem = seller[i];
-
+            int nowMon = amount[i];
+            int comis = 0;
+            while (nowMem != "-" && nowMon > 0) {
+                comis = (int) Math.floor(nowMon / 10);
+                answer[memIdxMap.get(nowMem)] += nowMon-comis;
+                nowMem = parentMap.get(nowMem);
+                nowMon = comis;
+            }
         }
 
         return answer;
