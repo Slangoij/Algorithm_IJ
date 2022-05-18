6338221be6eb0384053fad4065d6232fb73dfc3b
slangoij <71580318+Slangoij@users.noreply.github.com>
2022-05-18 17:59:19 +0900

    알고풀이-실패율_220518

---
+++ JAVA/Programmers/Kakao2019Blind/실패율.java
@@ -0,0 +1,17 @@
+package Programmers.Kakao2019Blind;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class 실패율 {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+        ArrayList<Integer>
+        for (int i = 0; i < stages.length; i++) {
+
+        }
+        Collections.sort();
+
+        return answer;
+    }
+}
--- JAVA/Solution.java
+++ JAVA/Solution.java
@@ -1,18 +1,17 @@
 import Programmers.DevMatching2021WebBackend.다단계칫솔판매;
+import Programmers.Kakao2019Blind.실패율;
 
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        다단계칫솔판매 solu = new 다단계칫솔판매();
+        실패율 solu = new 실패율();
 
-        String[] enroll = {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"};
-        String[] referral = {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"};
-        String[] seller = {"young", "john", "tod", "emily", "mary"};
-        int[] amount = {12, 4, 2, 5, 10};
+        int N;
+        int[] stages;
 
         System.out.println(Arrays.toString(solu.solution(
-                enroll, referral, seller, amount
+                N, stages
         )));
     }
 }
