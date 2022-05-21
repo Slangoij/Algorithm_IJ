11a4458ce20b7d7bb1d04cded585b12286d4f7fc
ij_slango <junginje@gmail.com>
2022-05-21 14:43:41 +0900

    N으로표현 풀이중_220521

--- Python/Programmers/DynamicProgramming/N으로표현.py
+++ Python/Programmers/DynamicProgramming/N으로표현.py
@@ -1,3 +1,35 @@
+import copy
+
+def oper(ival, jval):
+    lst = [ival + jval, ival * jval]
+    if ival >= jval:
+        lst.append(ival - jval)
+    if jval != 0:
+        lst.append(ival // jval)
+    return lst
+
+def solution(N, number):
+    dp = [0, [N]]
+    chkset = set([N])
+    for i in range(1,9):
+        tmpset = copy.deepcopy(chkset)
+        for j in range(1,i+1):
+            k = i+1-j
+            for dpj in dp[j]:
+                for dpk in dp[k]:
+                    tmpset.update(oper(dpj, dpk))
+        if number in tmpset:
+            return i
+        chkset.update(tmpset)
+        
+    return -1
+
+# print(solution(1,1))
+print(solution(5,12))
+
+"""
+# 1번째 시도: 이전 단계의 숫자들에만 한번 연산 시도
+
 def solution(N, number):
     vstd = [False for _ in range(number*N+1)]
     dp = [0, [N]]
@@ -19,6 +51,4 @@ def solution(N, number):
                     tmpnxt.append(k)
         dp.append(tmpnxt)
     return -1
-
-# print(solution(1,1))
-print(solution(5,12))
+"""
