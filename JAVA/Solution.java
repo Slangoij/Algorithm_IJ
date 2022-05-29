6944e78c5ddea1f61b19e3fcb1fd7d70a0b2491b
ij_slango <junginje@gmail.com>
2022-05-29 23:15:47 +0900

    알고풀이_단체사진찍기 풀이완료_220529

--- JAVA/Solution.java
+++ JAVA/Solution.java
@@ -7,8 +7,8 @@ public class Solution {
         단체사진찍기 solu = new 단체사진찍기();
 
         int n = 2;
-//        String[] data = {"N~F=0", "R~T>2"};
-        String[] data = {"M~C<2", "C~M>1"};
+        String[] data = {"N~F=0", "R~T>2"};
+//        String[] data = {"M~C<2", "C~M>1"};
 //        String[] data = {"N~F=0"};
         System.out.println(solu.solution(n, data));
     }
