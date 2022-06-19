d9101b024b7179aa6b365610e13abc456bc9f30c
ij_slango <junginje@gmail.com>
2022-06-19 22:01:32 +0900

    알고풀이-괄호회전하기 풀이중-220619

--- JAVA/Solution.java
+++ JAVA/Solution.java
@@ -1,13 +1,12 @@
-
-import Programmers.조이스틱;
+import Programmers.MonthlyCodeChallenge2.괄호회전하기;
 
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        조이스틱 solu = new 조이스틱();
+        괄호회전하기 solu = new 괄호회전하기();
 
-        String s = "JAN";
+        String s = 	"}}}";
         System.out.println(solu.solution(s));
     }
 }
