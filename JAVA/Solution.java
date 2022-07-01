b5dd5b7261128358e337ff3e3aa195b4c648e8e3
ij_slango <junginje@gmail.com>
2022-07-01 22:52:38 +0900

    알고풀이-위장 풀이완료-220701

--- JAVA/Solution.java
+++ JAVA/Solution.java
@@ -1,12 +1,16 @@
-import Programmers.MonthlyCodeChallenge2.괄호회전하기;
+import Programmers.위장;
 
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        괄호회전하기 solu = new 괄호회전하기();
+        위장 solu = new 위장();
 
-        String s = 	"}}}";
+        String[][] s = {
+                {"yellow_hat", "headgear"},
+                {"blue_sunglasses", "eyewear"},
+                {"green_turban", "headgear"}
+        };
         System.out.println(solu.solution(s));
     }
 }
