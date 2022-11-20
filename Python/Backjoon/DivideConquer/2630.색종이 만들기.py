04fb1989cdfd9e10be5940919f7c65081bf69a23
ij_slango <junginje@gmail.com>
2022-11-20 21:27:32 +0900

    Update 색종이만들기

--- Python/Backjoon/DivideConquer/2630.색종이 만들기.py
+++ Python/Backjoon/DivideConquer/2630.색종이 만들기.py
@@ -1,12 +1,11 @@
 main_idx = int(input())
 
-answer = 0
-posmap = [
-    (0,0), (0,1), (1,0), (1,1)
-]
+answer = [0, 0]
+posmap = [ (0,0), (0,1), (1,0), (1,1) ]
 orig_map = []
 
 def chk_all_data(map):
+    global answer
     tmpval = map[0][0]
     for row in map:
         for data in row:
@@ -14,33 +13,34 @@ def chk_all_data(map):
                 return False
     return True
 
-def get_crnt_map(map):
-    pass
+def main_recur(i, j, main_idx):
+    global answer
 
-def main_recur(i, j):
-    global answer, main_idx
-    for quadpos in posmap:
-        tmpmap = []
-        crnt_pos_i = int(i + main_idx*quadpos[0])
-        crnt_pos_j = int(j + main_idx*quadpos[1])
-        for tmpi in range(crnt_pos_i, crnt_pos_i + main_idx):
-            tmprow = []
-            for tmpj in range(crnt_pos_j, crnt_pos_j + main_idx):
-                tmprow.append(orig_map[tmpi][tmpj])
-            tmpmap.append(tmprow)
+    tmpmap = []
+    for tmpi in range(i, i + main_idx):
+        tmprow = []
+        for tmpj in range(j, j + main_idx):
+            tmprow.append(orig_map[tmpi][tmpj])
+        tmpmap.append(tmprow)
 
-        if chk_all_data(tmpmap):
-            answer += 1
-        else:
-            main_idx = main_idx//2
-            main_recur(crnt_pos_i, crnt_pos_j)
+    if chk_all_data(tmpmap):
+        answer[tmpmap[0][0]] += 1
+    else:
+        main_idx = main_idx//2
+        for quadpos in posmap:
+            nxt_i = i + main_idx * quadpos[0]
+            nxt_j = j + main_idx * quadpos[1]
+            main_recur(nxt_i, nxt_j, main_idx)
+    
+    print(answer)
+        
 
 if __name__ == "__main__":
     for _ in range(main_idx):
         tmp_row = list(map(int, input().split()))
         orig_map.append(tmp_row)
 
-    main_recur(0, 0)
+    main_recur(0, 0, main_idx)
     print(answer)
 
 
