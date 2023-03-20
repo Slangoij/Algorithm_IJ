c44172533c85ea2a6a85fcad1970cd2a2c290cba
ij_slango <junginje@gmail.com>
2023-03-21 06:15:24 +0900

    11729_하노이탑 풀이중

--- Python/Backjoon/DP/11729.하노이탑 이동 순서.py
+++ Python/Backjoon/DP/11729.하노이탑 이동 순서.py
@@ -1,8 +1,16 @@
-def move_hanoi(n):
-    if n // 2 == 0:
-        pass
-    else:
-        pass
+count = 0
+
+def mv1()
+
+def move_hanoi(m, N):
+    if m == 1:
+        print()
+        count += 1
+        
+    move_hanoi(m-1, N)
+    print(m)
+    move_hanoi(m-1, N)
 
 if __name__ == "__main__":
-    
+    N = int(input())
+    move_hanoi(N)
